b714ac35d2300ce4855e9bd15c723c0339cb7a19
devferx <ferkki@hotmail.com>
2021-05-29 16:53:15 -0400

    :lipstick: [ADD] Login Styles

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,6 +1,19 @@
+import spotifuLogo from "../../assets/icons/spotifu-logo.svg";
+
+import "./styles.css";
+
 const AUTH_URL =
   "https://accounts.spotify.com/authorize?client_id=5d62507559f04b45a3a44e033520a057&response_type=code&redirect_uri=http://localhost:3000/home&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
 
 export const Login = () => {
-  return <div></div>;
+  return (
+    <main className="login">
+      <section className="login-card">
+        <img src={spotifuLogo} alt="Spotify Logo" />
+        <a className="button button-text" href={AUTH_URL}>
+          Login
+        </a>
+      </section>
+    </main>
+  );
 };
